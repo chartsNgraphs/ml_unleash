1b3b077a373e857cfe42e3e36db9287f10e7f8d3
Dwayne Thomson <dwaynethomson14@gmail.com>
2022-06-04 15:29:24 -0400

    re-added setup.py

---
+++ ml_unleash/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.0.1'
+DESCRIPTION = 'Package and run machine learning models as docker containers.'
+LONG_DESCRIPTION = 'Python package for packaging and deploying machine learning models as invokable endpoints in Docker containers.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'ml_unleash'
+        name="ml_unleash",
+        version=VERSION,
+        author="Dwayne Thomson",
+        author_email="dwaynethomson14@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["Flask"], # add any additional packages that
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'docker', 'mlops'],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
